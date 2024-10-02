e850fe5d5df5f928d8338cd31547da8069208f2f
Aleksander <olek@example.com>
2024-10-02 10:48:19 +0200

    dodanie pytania o imie nadawcy

--- praca_domowa1.py
+++ praca_domowa1.py
@@ -3,3 +3,5 @@ imie_odbiorcy = input("Podaj imie solenizanta/ solenizantki: ")
 rok_urodzenia = int(input("Podaj rok urodzenia solenizanta/ solenizantki: "))
 
 wiadomosc = input("Napisz wiadomość dla solenizanta/ solenizantki: ")
+
+imie_nadawcy = input("Podaj swoje imię: ")

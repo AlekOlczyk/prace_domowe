bc93786e37180fda24e79f89e46da043d40c9982
Aleksander <olek@example.com>
2024-10-02 10:46:42 +0200

    dodanie pytania o spersonalizowana wiadomosc

--- praca_domowa1.py
+++ praca_domowa1.py
@@ -1,3 +1,5 @@
 imie_odbiorcy = input("Podaj imie solenizanta/ solenizantki: ")
 
 rok_urodzenia = int(input("Podaj rok urodzenia solenizanta/ solenizantki: "))
+
+wiadomosc = input("Napisz wiadomość dla solenizanta/ solenizantki: ")
